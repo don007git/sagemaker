1451a505039868f2cf48c8ec3a40b72ac645b1b2
EC2 Default User <ec2-user@ip-172-16-79-167.ec2.internal>
2019-04-25 17:59:23 +0000

    first commit

---
+++ mygitpush.sh
@@ -0,0 +1,6 @@
+echo "# sagemaker" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/don007git/sagemaker.git
+git push -u origin master
